d5aac8898fe9bc648e723f31dd61284c59fa4fbc
Sergio <sergio@demarque.com>
2024-05-20 11:33:58 -0400

    wip

---
+++ web/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <!--HelloWorld msg="You did it!" /-->
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+
+  <div class="main-container">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.main-container {
+  width: 1020px;
+}
+</style>
---
+++ web/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h2>This is a home page</h2>
+
+    <select class="form-control select select-primary select-block mbl">
+      <optgroup label="Profile">
+        <option value="0">My Profile</option>
+        <option value="1">My Friends</option>
+      </optgroup>
+      <optgroup label="System">
+        <option value="2">Messages</option>
+        <option value="3">My Settings</option>
+        <option value="4">Logout</option>
+      </optgroup>
+    </select>
+  </div>
+</template>
+<style scoped></style>
