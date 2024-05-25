03532b4bf1bb89a5b8848e345c699bbc469061e9
Sergio <sergio@demarque.com>
2024-05-25 15:25:20 -0400

    using structured data in scraping

--- stations/src/index.ts
+++ stations/src/index.ts
@@ -16,28 +16,33 @@ async function current(): Promise<Response> {
   let id: string | null = "";
   let name: string = "";
   let geocoordinatesString: any;
+  let availabilityString: any;
+
   let stations: {
-    id: number;
-    bikes: number;
-    free_docks: number;
-    name: string;
-    lat?: number;
-    long?: number;
-  }[] = [];
+    [key: number]: {
+      id: number;
+      bikes?: number;
+      free_docks?: number;
+      name: string;
+      lat?: number;
+      long?: number;
+    };
+  } = {};
+
   const response = await fetch("https://aveloquebec.ca/stations/");
-  // instead of all of this, we could instead use '<input type='hidden' id='icon_txt' value=....', that seems to contain the JSON data
-  // for availability
   const rewriter = new HTMLRewriter()
+    // Getting id and name of the stations.
     .on('ul[id="infoWind"] > li', {
       element(el) {
         id = el.getAttribute("id");
         el.onEndTag(() => {
-          stations.push({
+          let data = {
             id: parseInt(id || ""),
             name: name.trim(),
             bikes: 0,
             free_docks: 0,
-          });
+          };
+          stations[data.id] = data;
           name = "";
         });
       },
@@ -47,14 +52,39 @@ async function current(): Promise<Response> {
         name = name + txt.text;
       },
     })
-    .on('input[name="arr_adr"]', {
+    .on('input[name="arr_adr"][type="hidden"]', {
       element(el) {
         geocoordinatesString = el.getAttribute("value");
       },
+    })
+    .on('input[id="icon_txt"][type="hidden"]', {
+      element(el) {
+        availabilityString = el.getAttribute("value");
+      },
     });
   await consume(rewriter.transform(response).body!);
+  // parse availability
+  if (availabilityString != null) {
+    let data = JSON.parse(availabilityString);
+    for (let key in data) {
+      let stationId = parseInt(key);
+      let [bikes, free_docks] = data[key].split("/");
+      stations[stationId].bikes = parseInt(bikes);
+      stations[stationId].free_docks = parseInt(free_docks);
+    }
+  }
+  // parse geocoordinates
+  if (geocoordinatesString != null) {
+    let data = JSON.parse(geocoordinatesString);
+    for (let key in data) {
+      let stationId = parseInt(key);
+      let [lat, long] = data[key].split("_");
+      stations[stationId].lat = Number(lat);
+      stations[stationId].long = Number(long);
+    }
+  }
 
-  return Response.json(stations);
+  return Response.json(Object.values(stations));
 }
 
 // ROUTING
