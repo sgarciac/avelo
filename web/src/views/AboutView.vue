eb351cfbd9962bbcb355ab844ce2761c7d32b526
Sergio <sergio@demarque.com>
2024-05-27 08:51:44 -0400

    about

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Ce site n'est pas affilié à àVélo.</div>
+  <p>Ce site n'est pas affilié à àVélo.</p>
+  <p>
+    Code source: <a href="https://github.com/sgarciac/avelo">https://github.com/sgarciac/avelo</a>
+  </p>
 </template>
 
 <style></style>
