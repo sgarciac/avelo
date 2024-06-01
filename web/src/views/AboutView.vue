6e7628bb49ff2cd78648270676c31785b2a86f90
Sergio <sergio@demarque.com>
2024-06-01 10:01:38 -0400

    credits

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="w-full flex flex-col items-center">
-    <div class="w-full prose my-auto">
+    <div class="w-full prose my-auto p-1">
       <p>Ce site n'est pas affilié à àVélo.</p>
       <p>
         Code source:
         <a href="https://github.com/sgarciac/avelo">https://github.com/sgarciac/avelo</a>
       </p>
+      <p>Contributeurs:</p>
+      <p>
+        Sergio Garcia
+        <a href="https://www.github.com/sgarcia">https://www.github.com/sgarcia</a>
+      </p>
+      <p>
+        Marc Falla
+        <a href="https://www.github.com/sgarcia">https://www.github.com/mfalla</a>
+      </p>
+
       <p>Commentaires: sergio.garcia@gmail.com</p>
     </div>
   </div>
